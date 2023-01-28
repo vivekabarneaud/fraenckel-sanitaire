63e5e79bff4baecad5fa22378e55f6d35ab5e994
vivekabarneaud <viveka.barneaud@aaaic.com>
2023-01-28 19:25:52 +0100

    responsive contact div

--- src/components/TroubleshootCard.svelte
+++ src/components/TroubleshootCard.svelte
@@ -1,12 +1,12 @@
-<div class="flex h-80 w-full bg-darkerBlue shadow-xl text-white">
+<div class="flex justify-center lg:justify-start h-80 w-full bg-darkerBlue shadow-xl text-white">
   <img src="/images/troubleshooting-img.jpeg" alt="plombier dépanneur" class="hidden lg:block lg:h-full"/>
   <div class="flex flex-col justify-between p-8">
     <div class="flex flex-col items-center lg:items-start gap-y-6">
       <h3 class="text-4xl">Urgences et dépannage</h3>
       <p class="max-w-md text-sm opacity-80 text-center lg:text-start">Dépannage 7j/7 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec massa sapien. Augue eget arcu dictum varius.</p>
     </div>
-    <div class="flex items-center gap-x-6 pl-24 max-w-md rounded-lg bg-dangerColor">
-      <img src="/images/ClipartKey_1722102.png" alt="icône téléphone" class="w-8" />
+    <div class="flex items-center justify-center lg:justify-start gap-x-6 lg:px-[20%] max-w-md rounded-lg bg-dangerColor">
+      <img src="/images/ClipartKey_1722102.png" alt="icône téléphone" class="hidden lg:block w-8" />
       <div class="flex flex-col gap-y-1 items-center justify-center py-4">
         <h5 class="opacity-80 text-xs">CONTACTEZ-NOUS</h5>
         <span class="text-2xl">07 81 79 10 69</span>
