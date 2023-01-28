d181c3267249e938e87e0e9cf7f10c128636bb58
vivekabarneaud <viveka.barneaud@aaaic.com>
2023-01-28 20:09:56 +0100

    added projects card

---
+++ src/components/ProjectsCard.svelte
@@ -0,0 +1,11 @@
+<div class="flex justify-center lg:justify-end h-96 w-full bg-darkerBlue shadow-xl text-white">
+  <div class="flex flex-col justify-between p-8">
+    <div class="flex flex-col items-center lg:items-start gap-y-6">
+      <h3 class="text-4xl">Projets et rénovations</h3>
+      <p class="max-w-md text-sm opacity-80 text-center lg:text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec massa sapien. Augue eget arcu dictum varius.</p>
+    </div>
+    <button class="rounded-lg bg-regularBlue px-6 py-2 hover:-translate-y-0.5 hover:bg-lightBlue active:bg-darkBlue transition-all duration-0.2">DEMANDER UN DEVIS</button>
+  </div>
+  <img src="/images/bathroom-1.jpg" alt="bathroom" class="hidden lg:block lg:h-full"/>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
 
 import Banner from "../components/Banner.svelte";
 import TroubleshootCard from "../components/TroubleshootCard.svelte";
+import ProjectsCard from "../components/ProjectsCard.svelte";
 </script>
 
 <div class="flex flex-col gap-y-8 min-h-screen w-screen bg-backgroundColor">
   <Banner />
   <TroubleshootCard />
+  <ProjectsCard />
 </div>
